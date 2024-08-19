0e2db2fd14211d3aee290a62a40e9429f7a5f707
Raphael Martins <raphael.chiuso@gmail.com>
2024-08-19 19:47:44 -0300

    refactor: updated project to use viewBinding

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -2,37 +2,30 @@ package com.example.cadastro
 
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
-import android.widget.RadioGroup
-import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.Cadastro.Formulario
+import com.example.cadastro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val editTextNome: EditText = findViewById(R.id.editTextNome)
-        val editTextTelefone: EditText = findViewById(R.id.editTextTelefone)
-        val editTextEmail: EditText = findViewById(R.id.editTextEmail)
-        val checkBoxEmailList: CheckBox = findViewById(R.id.checkBoxEmailList)
-        val radioGroupSexo: RadioGroup = findViewById(R.id.radioGroupSexo)
-        val editTextCidade: EditText = findViewById(R.id.editTextCidade)
-        val spinnerEstado: Spinner = findViewById(R.id.spinnerEstado)
-        val buttonLimpar: Button = findViewById(R.id.buttonLimpar)
-        val buttonSalvar: Button = findViewById(R.id.buttonSalvar)
 
         val estados = arrayOf(
             "Selecione",
@@ -65,20 +58,20 @@ class MainActivity : AppCompatActivity() {
         )
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, estados)
-        spinnerEstado.adapter = adapter
+        binding.spinnerEstado.adapter = adapter
 
-        buttonSalvar.setOnClickListener {
-            val nome = editTextNome.text.toString()
-            val telefone = editTextTelefone.text.toString()
-            val email = editTextEmail.text.toString()
-            val ingressarNaLista = checkBoxEmailList.isChecked
-            val sexo = when (radioGroupSexo.checkedRadioButtonId) {
+        binding.buttonSalvar.setOnClickListener {
+            val nome = binding.editTextNome.text.toString()
+            val telefone = binding.editTextTelefone.text.toString()
+            val email = binding.editTextEmail.text.toString()
+            val ingressarNaLista = binding.checkBoxEmailList.isChecked
+            val sexo = when (binding.radioGroupSexo.checkedRadioButtonId) {
                 R.id.radioButtonMasculino -> "Masculino"
                 R.id.radioButtonFeminino -> "Feminino"
                 else -> "Não especificado"
             }
-            val cidade = editTextCidade.text.toString()
-            val estado = spinnerEstado.selectedItem.toString()
+            val cidade = binding.editTextCidade.text.toString()
+            val estado = binding.spinnerEstado.selectedItem.toString()
 
             val formulario =
                 Formulario(nome, telefone, email, ingressarNaLista, sexo, cidade, estado)
@@ -86,14 +79,14 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, formulario.toString(), Toast.LENGTH_LONG).show()
         }
 
-        buttonLimpar.setOnClickListener {
-            editTextNome.text.clear()
-            editTextTelefone.text.clear()
-            editTextEmail.text.clear()
-            checkBoxEmailList.isChecked = false
-            radioGroupSexo.clearCheck()
-            editTextCidade.text.clear()
-            spinnerEstado.setSelection(0)
+        binding.buttonLimpar.setOnClickListener {
+            binding.editTextNome.text.clear()
+            binding.editTextTelefone.text.clear()
+            binding.editTextEmail.text.clear()
+            binding.checkBoxEmailList.isChecked = false
+            binding.radioGroupSexo.clearCheck()
+            binding.editTextCidade.text.clear()
+            binding.spinnerEstado.setSelection(0)
         }
     }
 }
